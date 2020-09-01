867e3bda2292d72b41fff5784a5ede2e1ad47527
Salient Systems Corporation <mobile.salientsys@gmail.com>
2020-08-31 20:07:58 -0500

    Fix Package.swift for SPM 5.2

--- Package.swift
+++ Package.swift
@@ -1,5 +1,28 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "CGOperators"
+    name: "CGOperators",
+    platforms: [
+        .iOS(.v9),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "CGOperators",
+            targets: ["CGOperators"])
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "CGOperators",
+            dependencies: [],
+            path: "Source")
+    ],
+    swiftLanguageVersions: [.v5]
 )
